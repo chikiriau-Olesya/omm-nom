c6f360ba235fdd299f9f6000ddc148c8ee8c7816
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-06-05 11:39:21 +0300

    wip events

--- js/events.js
+++ js/events.js
@@ -31,11 +31,15 @@ function windowShow(element) {
     element.classList.add('window__item-active')
 }
 
-// let openShop = document.getElementById('openShop')
-// let openShopWindow = document.getElementById('openShopWindow')
-// openShop.onclick = function() {
-//     windowShow(openShopWindow)
+// function chooseClassWindow(el) {
+//     for(let i = 0; i < el.length; i++) {
+//         el[i].onclick = function() {
+//             windowShow()
+//         }
+//     }
 // }
+
+//first line
 
 let openShop = document.querySelectorAll('.openShop-btn')
 let openShopWindow = document.getElementById('openShopWindow')
@@ -45,12 +49,6 @@ for(let i = 0; i < openShop.length; i++) {
     }
 }
 
-// let talk = document.getElementById('talk')
-// let talkWindow = document.getElementById('talkWindow')
-// talk.onclick = function() {
-//     windowShow(talkWindow)
-// }
-
 let talk = document.querySelectorAll('.talk-btn')
 let talkWindow = document.getElementById('talkWindow')
 for(let i = 0; i < talk.length; i++) {
@@ -59,6 +57,46 @@ for(let i = 0; i < talk.length; i++) {
     }
 }
 
+let lecture = document.querySelectorAll('.lecture-btn')
+let lectureWindow = document.getElementById('lectureWindow')
+for(let i = 0; i < lecture.length; i++) {
+    lecture[i].onclick = function() {
+        windowShow(lectureWindow)
+    }
+}
 
+//second line
+let newProduct = document.querySelectorAll('.new-product-btn')
+let newProductWindow = document.getElementById('newProductWindow')
+for(let i = 0; i < newProduct.length; i++) {
+    newProduct[i].onclick = function() {
+        windowShow(newProductWindow)
+    }
+}
+
+//third line
+let candy = document.querySelectorAll('.candy-btn')
+let candyWindow = document.getElementById('candyWindow')
+for(let i = 0; i < candy.length; i++) {
+    candy[i].onclick = function() {
+        windowShow(candyWindow)
+    }
+}
+
+let public  = document.querySelectorAll('.public-btn')
+let publicWindow = document.getElementById('publicWindow')
+for(let i = 0; i < public.length; i++) {
+    public[i].onclick = function() {
+        windowShow(publicWindow)
+    }
+}
+
+let meet = document.querySelectorAll('.meet-btn')
+let meetWindow = document.getElementById('meetWindow')
+for(let i = 0; i < meet.length; i++) {
+    meet[i].onclick = function() {
+        windowShow(meetWindow)
+    }
+}
 
 
