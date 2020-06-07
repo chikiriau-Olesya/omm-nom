ea6c547b7237083c370073cd1599495a92ff85ee
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-06-07 13:14:50 +0300

    WIP catalog circ anime

--- js/catalog.js
+++ js/catalog.js
@@ -1,43 +1,22 @@
 
 
-const circle = document.querySelectorAll('.circle')
+const circle = document.querySelector('.circleSpe')
 
-for (let i = 0; i < circle.length; i++) {
-    let circleAnim = anime({
-        targets: circle,
-        translateX: anime.random(20, 95) + 'vw',
-        translateY:[
-            { 
-                value: anime.random(2, 9) + 'vw', 
-                duration: 2500 
-            },
-            { 
-                value: anime.random(3, 8) + 'vw', 
-                duration: 2500, 
-            },
-        ],
-        duration: anime.random(5000, 6000),
-        direction: 'alternate',
-        loop: true,
-        easing: 'easeInOutSine',
-    })
-}
-
-// let circleAnim = anime({
-//     targets: circle,
-//     translateX: anime.random(20, 95) + 'vw',
-//     translateY:[
-//         { 
-//             value: anime.random(2, 9) + 'vw', 
-//             duration: 2500 
-//         },
-//         { 
-//             value: anime.random(3, 8) + 'vw', 
-//             duration: 2500, 
-//         },
-//     ],
-//     duration: anime.random(5000, 6000),
-//     direction: 'alternate',
-//     loop: true,
-//     easing: 'easeInOutSine',
-// })
+let circleAnim = anime({
+    targets: circle,
+    translateX: anime.random(20, 95) + 'vw',
+    translateY:[
+        { 
+            value: anime.random(2, 9) + 'vw', 
+            duration: 2500 
+        },
+        { 
+            value: anime.random(3, 8) + 'vw', 
+            duration: 2500, 
+        },
+    ],
+    duration: anime.random(5000, 6000),
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine',
+})
