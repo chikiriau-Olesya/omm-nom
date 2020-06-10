b396d2848b4f23be566fad5e74cb85e4043e3144
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-06-10 13:52:57 +0300

    wip animation

---
+++ js/card.js
@@ -0,0 +1,14 @@
+// $(document).ready( () => {
+// 	$('button.jGravity').on('click', () => { 
+// 		$('body').jGravity({
+// 			target: 'everything',
+// 			ignoreClass: 'ignoreMe',
+// 			weight: 25, 
+// 			depth: 0.1,
+// 			drag: true ,
+// 		});
+
+// 	});
+// });
+
+
